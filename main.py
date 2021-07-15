bac3180d288aa756b4f154a5a1012392b84395de
Egg-Fish <49740285+Egg-Fish@users.noreply.github.com>
2021-07-15 22:55:03 +0800

    feat: more debug statements

--- main.py
+++ main.py
@@ -44,27 +44,74 @@ def addGame(gamecode,
         "SocketController": socketcontroller
     }
 
-def createTestGame():
-    addGame("oriontestgame", socketio)
+def testMessages():
+    addGame("testmessages", socketio)
 
-    testPlayer = Player("TEST")
-    testPlayer.setAlias("TEST")
-
+    # 1) Players join the game
     for i in range(1, 7):
         dummy = Player(f"dummy{i}")
-        GAMES["oriontestgame"]["GameController"].addPlayer(dummy)
+        GAMES["testmessages"]["GameController"].addPlayer(dummy)
 
-    GAMES["oriontestgame"]["GameController"].addMessage(testPlayer, "Testing Public Message")
+    # 2) Game Starts
+    GAMES["testmessages"]["GameController"].startGame()
+    # 2.1) This will not appear
+    GAMES["testmessages"]["GameController"].handleMessage("maliciousfellow", "This will not appear")
+    # 2.2) And these will appear in the respective chats
+    GAMES["testmessages"]["GameController"].handleMessage("dummy1", "This will appear on the hacker chat, but will be erased")
+    GAMES["testmessages"]["GameController"].handleMessage("dummy2", "This will appear on the whitehat chat, but will be erased")
+    GAMES["testmessages"]["GameController"].handleMessage("dummy3", "This will appear on the investigator chat, but will be erased")
+    GAMES["testmessages"]["GameController"].handleMessage("dummy4", "This wouldve appeared on the civilian chat, but its nighttime")
+    GAMES["testmessages"]["GameController"].handleMessage("dummy1", "/t 2")
+    GAMES["testmessages"]["GameController"].handleMessage("dummy1", "/t -1")
+    GAMES["testmessages"]["GameController"].handleMessage("dummy1", "/t 0")
+    GAMES["testmessages"]["GameController"].handleMessage("dummy1", "/t 3")
 
-    printGameInstance(GAMES["oriontestgame"]["GameInstance"])
-    printGameController(GAMES["oriontestgame"]["GameController"])
+    GAMES["testmessages"]["GameController"].handleMessage("dummy2", "/p 3")
+    GAMES["testmessages"]["GameController"].handleMessage("dummy2", "/p -1")
+    GAMES["testmessages"]["GameController"].handleMessage("dummy2", "/p 0")
+    GAMES["testmessages"]["GameController"].handleMessage("dummy1", "/p 3")
+
+    GAMES["testmessages"]["GameController"].handleMessage("dummy3", "/s 2")
+    GAMES["testmessages"]["GameController"].handleMessage("dummy3", "/s -1")
+    GAMES["testmessages"]["GameController"].handleMessage("dummy3", "/s 0")
+    GAMES["testmessages"]["GameController"].handleMessage("dummy1", "/s 3")
+
+    printGameController(GAMES["testmessages"]["GameController"])
+    # time.sleep(3)
+
+    GAMES["testmessages"]["GameController"].updateGame()
+    # 3) Night Ends. Everyone can talk
+    GAMES["testmessages"]["GameController"].handleMessage("dummy1", "This will appear from dummy1's alias")
+    
+    GAMES["testmessages"]["GameController"].handleMessage("dummy2", "This will appear from dummy2's alias")
+    GAMES["testmessages"]["GameController"].handleMessage("dummy3", "This will appear from dummy3's alias")
+    GAMES["testmessages"]["GameController"].handleMessage("dummy4", "Now I can talk because it is daytime")
+    GAMES["testmessages"]["GameController"].handleCommand("GAMEMASTER", "sendMessage:Gamemasters can talk to everyone")
+    GAMES["testmessages"]["GameController"].handleMessage("andsomeone", "who is not in the game cannot talk")
+    GAMES["testmessages"]["GameController"].handleCommand("andneither", "can they send commands")
+
+    GAMES["testmessages"]["GameController"].handleMessage("dummy1", "/v 0")
+    GAMES["testmessages"]["GameController"].handleMessage("dummy1", "/v 1")
+    GAMES["testmessages"]["GameController"].handleMessage("dummy2", "/v 4")
+    GAMES["testmessages"]["GameController"].handleMessage("dummy3", "/v 1")
+    GAMES["testmessages"]["GameController"].handleMessage("dummy4", "/v 1")
+    GAMES["testmessages"]["GameController"].handleMessage("dummy5", "/v 5")
+    GAMES["testmessages"]["GameController"].handleMessage("dummy6", "/v 5")
+
+    printGameController(GAMES["testmessages"]["GameController"])
+    # time.sleep(3)
+
+    GAMES["testmessages"]["GameController"].updateGame()
+
+    printGameInstance(GAMES["testmessages"]["GameInstance"])
+    printGameController(GAMES["testmessages"]["GameController"])
 
 app = Flask(__name__)
 app.config["SECRET_KEY"] = os.urandom(24)
 
 socketio = SocketIO(app, async_mode="eventlet")
 
-createTestGame()
+testMessages()
 
 
 if __name__=="__main__":
