a110091275961ff01969266fd069cf11a1c94cff
Egg-Fish <49740285+Egg-Fish@users.noreply.github.com>
2021-07-20 11:15:12 +0800

    feat: added licensing text

--- gameinstance.py
+++ gameinstance.py
@@ -1,4 +1,21 @@
 """
+This file is part of Hacker.
+
+Hacker is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 3 of the License, or
+(at your option) any later version.
+
+Hacker is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with Hacker.  If not, see <https://www.gnu.org/licenses/>.
+"""
+
+"""
 File: gameinstance.py
 This file contains the implementation of the GameInstance Class.
 
